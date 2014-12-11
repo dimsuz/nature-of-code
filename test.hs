823857386590ebcc21c6be16a67e787411cc4c95
Dmitry Suzdalev <dimsuz@gmail.com>
2014-12-11 02:03:48 +0200

    Added a timeout and state update function
    Will use it as a basis for animation later

--- test.hs
+++ test.hs
@@ -11,7 +11,17 @@ renderFigure canvas= do
   return True
 
 figure :: Diagram Cairo R2
-figure =  unitCircle # scaleX 0.5 # rotateBy (1/6) # fc red
+figure =  circle 2
+
+
+type State = [Int]
+
+update :: DrawingArea -> State -> IO Bool
+update canvas state = do
+  putStrLn "updating"
+  -- TODO change state MVar here
+  widgetQueueDraw canvas
+  return True
 
 main :: IO ()
 main = do
@@ -24,4 +34,5 @@ main = do
               containerChild := canvas, containerBorderWidth := 8]
   onDestroy window mainQuit
   widgetShowAll window
+  timeoutAdd (update canvas [1..3]) 500
   mainGUI
