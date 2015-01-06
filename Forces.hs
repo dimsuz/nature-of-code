6954add69cdb1fd75e361ab5af92d2d99b7bce54
Dmitry Suzdalev <dimsuz@gmail.com>
2015-01-07 01:52:09 +0200

    Forces nice demo finished, multiple balls

--- Forces.hs
+++ Forces.hs
@@ -1,6 +1,7 @@
 import Graphics.Gloss.Interface.Pure.Game
 import Graphics.Gloss.Data.Vector
 import Graphics.Gloss
+import System.Random
 
 type MousePosition = Vector
 
@@ -10,7 +11,7 @@ data Mover = Mover { location :: Vector,
                      mass :: Float
                    }
 
-data World = World { mover :: Mover, mousePosition:: MousePosition, size :: Vector, debug :: String }
+data World = World { movers :: [Mover], mousePosition:: MousePosition, size :: Vector, debug :: String }
 
 normalizeV' (0,0) = (0,0)
 normalizeV' v = normalizeV v
@@ -18,14 +19,20 @@ normalizeV' v = normalizeV v
 limitV :: Vector -> Float -> Vector
 limitV v len = if magV v > len then len `mulSV` normalizeV' v else v
 
-drawCircle :: Float -> Vector -> Picture
-drawCircle r (x,y) = Translate x y $ Pictures [color (makeColor 0.5 0.5 0.5 1.0) $ circleSolid r,
-                                             color black $ thickCircle r 2]
+mapRange :: (Num a, Fractional a) => a -> (a,a) -> (a,a) -> a
+mapRange val (imin,imax) (omin,omax) =
+    omin + (omax - omin) * ((val - imin) / (imax - imin))
+
+drawCircle :: Float -> Float -> Vector -> Picture
+drawCircle rad m (x,y) = Translate x y $ Pictures [color (makeColor r g b 1.0) $ circleSolid rad,
+                                             color black $ thickCircle rad 2]
+                       where r = mapRange m (0,20) (0,1)
+                             g = mapRange m (0,20) (0,0.9)
+                             b = mapRange m (0,20) (0,0.3)
 
 draw :: World -> Picture
-draw world = Pictures [
-              drawCircle 20 $ location (mover world)
-             ]
+draw world = Pictures (map drawMover $ movers world)
+    where drawMover mover = drawCircle (2 * (mass mover)) (mass mover) (location mover)
 
 computeVelocity :: World -> Mover -> Mover
 computeVelocity world mover =  mover { velocity = vel }
@@ -54,35 +61,37 @@ applyForce force world mover = mover { acceleration = acc }
                                      m = mass mover
 
 update :: Float -> World -> World
-update time world@(World m mousePos sz dbg) = World {
-                                                   mover = newMover,
+update time world@(World ms mousePos sz dbg) = World {
+                                                   movers = newMovers,
                                                    mousePosition = mousePos,
                                                    size = sz,
                                                    debug = dbg }
-    where newMover = transition m
-                     where transition = foldr (.) id curried
-                           curried = map ($ world) transforms
-                           transforms = reverse
-                                        [
-                                          applyForce (0.01, 0),
-                                          applyForce (0.0, -1.0),
-                                          computeVelocity,
-                                          computeLocation,
-                                          bounceOffWalls,
-                                          resetForces
-                                        ]
+    where newMovers = map newMover ms
+          newMover m = transition m
+              where transition = foldr (.) id curried
+                    curried = map ($ world) transforms
+                    transforms = reverse
+                                 [
+                                  applyForce (0.01, 0),
+                                  applyForce (0.0, -1.0),
+                                  computeVelocity,
+                                  computeLocation,
+                                  bounceOffWalls,
+                                  resetForces
+                                 ]
 
 event :: Event -> World -> World
-event e world = case e of
-                  EventMotion v -> World (mover world) v (size world) (show e)
-                  otherwise -> world
+event _ world = world
 
-main
- = play (InWindow "GameEvent" (800, 600) (10, 10))
+mkMover m = Mover { location = (-400 + m*10, 0), velocity = (0,0), acceleration = (0.0,0.0), mass = m }
+
+main = do
+   stdGen <- getStdGen
+   play (InWindow "GameEvent" (800, 600) (10, 10))
         white
         100
         World {
-     mover = Mover { location = (0, 0), velocity = (0,0), acceleration = (0.0,0.0), mass = 10.0 },
+     movers = map mkMover $ take 50 (randomRs (5, 80) stdGen),
      mousePosition = (0,0), debug = "hello", size = (800, 600)
         }
         draw
